7fb00dac65c8ed691725ed4e7613200d047f7c3a
DivyaMadupalli <divyamadupally@gmail.com>
2021-03-22 10:26:37 +0530

    adding lab3

---
+++ Lab3/src/stringTokenizer.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class stringTokenizer {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the integers with space:");
+		String line=sc.nextLine();
+		StringTokenizer st=new StringTokenizer(line);
+		int sum=0,i=1;
+		while(st.hasMoreTokens())
+		{
+			int n=0;
+			n=Integer.parseInt(st.nextToken());
+			System.out.println("number " +n);
+			sum=sum+n;
+		}
+		System.out.println("sum"+sum);
+	}
+
+}
